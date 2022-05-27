2075af481d79a397e096fd088f75e2af971a1c8b
Priyanka Bai Kemavath <priyankabai.kemavath@hcl.com>
2022-05-27 14:58:04 +0530

    Resource page implemented

---
+++ src/fetchers/getFetchers.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getResourceDetails = async() => {
+    let url = 'http://localhost:3001/employee';
+    const response = await axios.get(url);
+    if(response.statusText !== "OK") {
+        throw new Error('Something went wrong!');
+    }
+    return response.data;
+};
