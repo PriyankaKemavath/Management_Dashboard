e111b712ab5afbbfb1221fd70c5098743d433ca8
Priyanka Bai Kemavath <priyankabai.kemavath@hcl.com>
2022-06-30 19:52:20 +0530

    Removed unused files

--- src/components/About/About.js
+++
@@ -1,9 +0,0 @@
-import classes from './About.module.css';
-
-const About = () => {
-    return (
-        <h4 className={classes.content}>In Progress...</h4>
-    );
-};
-
-export default About;
--- src/components/FulfillmentTracker/FulfillmentTracker.js
+++
@@ -1,7 +0,0 @@
-const FulfillmentTracker = () => {
-    return (
-        <h3>FulfillmentTracker</h3>
-    );
-};
-
-export default FulfillmentTracker;
