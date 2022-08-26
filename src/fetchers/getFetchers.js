7e40cff77f63588cb766dd6026f5f2ae38f495b1
Priyanka Bai Kemavath <priyankabai.kemavath@hcl.com>
2022-08-26 14:36:12 +0530

    Changed server api urls

--- src/fetchers/getFetchers.js
+++ src/fetchers/getFetchers.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 import { getConfiguration } from "../config";
 
 const BASE_URL = getConfiguration().apiUrl;
 
-export const getResourceDetails = async() => {
-    let url = 'http://localhost:3001/employee';
-    /* let url = `${BASE_URL}/employee`; */
-    const response = await axios.get(url);
-    if(response.statusText !== "OK") {
-        throw new Error('Something went wrong!');
-    }
-    return response.data;
+export const getResourceDetails = async () => {
+  /* let url = 'http://localhost:3001/employee'; */
+  let url = `${BASE_URL}/employee`;
+  const response = await axios.get(url);
+  if (response.statusText !== "OK") {
+    throw new Error("Something went wrong!");
+  }
+  return response.data;
 };
 
-export const getProjectDetails = async() => {
-    let url = 'http://localhost:3001/projects';
-    /* let url = `${BASE_URL}/projects`; */
-    const response = await axios.get(url);
-    if(response.statusText !== "OK") {
-        throw new Error('Something went wrong!');
-    }
-    return response.data;
-}
+export const getProjectDetails = async () => {
+  /* let url = 'http://localhost:3001/projects'; */
+  let url = `${BASE_URL}/projects`;
+  const response = await axios.get(url);
+  if (response.statusText !== "OK") {
+    throw new Error("Something went wrong!");
+  }
+  return response.data;
+};
